a6c9ff61344681af67f6ab2c25e9040b6f1372fd
Talita Bernardino <83371452+talibernardi@users.noreply.github.com>
2022-11-29 12:25:45 -0300

    validação peso-altura

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,57 @@ const data = [
       obesity: "III",
     },
   ];
+
+  const imcTable = document.querySelector("#imc-table");
+
+  const heightInput = document.querySelector("#height");
+  const weightInput = document.querySelector("#weight");
+  const calcBtn = document.querySelector("#calc-btn");
+  const clearBtn = document.querySelector("#clear-btn");
+
+  function createTable(data) {
+    data.forEach((item)=> {
+        const div = document.createElement("div");
+        div.classList.add("table-data");
+
+        const classification = document.createElement("p");
+        classification.innerText = item.classification;
+
+        const info = document.createElement("p");
+        info.innerText = item.info;
+
+        const obesity = document.createElement("p");
+        obesity.innerText = item.obesity;
+
+        div.appendChild(classification);
+        div.appendChild(info);
+        div.appendChild(obesity);
+
+        imcTable.appendChild(div);
+    });
+  }
+
+  function cleanInputs(){
+    heightInput.value = "";
+    weightInput.value = "";
+  }
+
+  function validDigits(text){
+    return text.replace(/[^0-9,]/g, "");
+  }
+
+  createTable(data);
+
+  [heightInput, weightInput].forEach((el) => {
+    el.addEventListener("input", (e) => {
+        const updateValue = validDigits(e.target.value);
+
+        e.target.value = updateValue;
+    });
+  });
+
+  clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    cleanInputs();
+  })
