33d5f28512498e0f39aa6b5b0c27985a112cc392
Talita Bernardino <83371452+talibernardi@users.noreply.github.com>
2023-01-09 15:10:33 -0300

    iniciando classificação

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,50 @@
 const data = [
     {
-      min: 0,
-      max: 18.4,
-      classification: "Menor que 18,5",
-      info: "Magreza",
-      obesity: "0",
+        min: 0,
+        max: 18.4,
+        classification: "Menor que 18,5",
+        info: "Magreza",
+        obesity: "0",
     },
     {
-      min: 18.5,
-      max: 24.9,
-      classification: "Entre 18,5 e 24,9",
-      info: "Normal",
-      obesity: "0",
+        min: 18.5,
+        max: 24.9,
+        classification: "Entre 18,5 e 24,9",
+        info: "Normal",
+        obesity: "0",
     },
     {
-      min: 25,
-      max: 29.9,
-      classification: "Entre 25,0 e 29,9",
-      info: "Sobrepeso",
-      obesity: "I",
+        min: 25,
+        max: 29.9,
+        classification: "Entre 25,0 e 29,9",
+        info: "Sobrepeso",
+        obesity: "I",
     },
     {
-      min: 30,
-      max: 39.9,
-      classification: "Entre 30,0 e 39,9",
-      info: "Obesidade",
-      obesity: "II",
+        min: 30,
+        max: 39.9,
+        classification: "Entre 30,0 e 39,9",
+        info: "Obesidade",
+        obesity: "II",
     },
     {
-      min: 40,
-      max: 99,
-      classification: "Maior que 40,0",
-      info: "Obesidade grave",
-      obesity: "III",
+        min: 40,
+        max: 99,
+        classification: "Maior que 40,0",
+        info: "Obesidade grave",
+        obesity: "III",
     },
-  ];
+];
 
-  const imcTable = document.querySelector("#imc-table");
+const imcTable = document.querySelector("#imc-table");
 
-  const heightInput = document.querySelector("#height");
-  const weightInput = document.querySelector("#weight");
-  const calcBtn = document.querySelector("#calc-btn");
-  const clearBtn = document.querySelector("#clear-btn");
+const heightInput = document.querySelector("#height");
+const weightInput = document.querySelector("#weight");
+const calcBtn = document.querySelector("#calc-btn");
+const clearBtn = document.querySelector("#clear-btn");
 
-  function createTable(data) {
-    data.forEach((item)=> {
+function createTable(data) {
+    data.forEach((item) => {
         const div = document.createElement("div");
         div.classList.add("table-data");
 
@@ -63,29 +63,58 @@ const data = [
 
         imcTable.appendChild(div);
     });
-  }
+}
 
-  function cleanInputs(){
+function cleanInputs() {
     heightInput.value = "";
     weightInput.value = "";
-  }
+}
 
-  function validDigits(text){
+function validDigits(text) {
     return text.replace(/[^0-9,]/g, "");
-  }
+}
 
-  createTable(data);
+function calcImc(weight, height) {
+    const imc = (weight / (height * height)).toFixed(1);
 
-  [heightInput, weightInput].forEach((el) => {
+    return imc;
+}
+
+createTable(data);
+
+[heightInput, weightInput].forEach((el) => {
     el.addEventListener("input", (e) => {
         const updateValue = validDigits(e.target.value);
 
         e.target.value = updateValue;
     });
-  });
+});
 
-  clearBtn.addEventListener("click", (e) => {
+calcBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const weight = +weightInput.value.replace(",", ".");
+    const height = +heightInput.value.replace(",", ".");
+
+    if (!weight || !height) return;
+
+    const imc = calcImc(weight, height);
+
+    let info;
+
+    data.forEach((item) => {
+        if (imc >= item.min && imc <= item.max) {
+            info = item.info;
+        }
+    });
+
+    console.log(info);
+
+    if (!info) return;
+});
+
+clearBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     cleanInputs();
-  })
+});
